9462e75028baad7728902f3deaf43f0cacd011cd
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-07-08 18:26:29 +0100

    debug menu options for going to next floor and starting on a specific floor

--- CharredMana/Assets/Scripts/Debug/DebugMenu.cs
+++ CharredMana/Assets/Scripts/Debug/DebugMenu.cs
@@ -1,5 +1,6 @@
+using System.Linq;
+using TMPro;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -12,6 +13,8 @@ public class DebugMenu : MonoBehaviour
     public class DebugOptions
     {
         public bool CanDie = true;
+        public bool StartOnFloor = false;
+        public int StartingFloor = 0;
     }
 
     [SerializeField] private GameObject _debugBuildText;
@@ -23,12 +26,18 @@ public class DebugMenu : MonoBehaviour
     [SerializeField] private Button _nextMusicButton;
     [SerializeField] private Button _goToMenuButton;
     [SerializeField] private Button _goToGameButton;
+    [SerializeField] private Toggle _startOnFloorToggle;
+    [SerializeField] private TMP_InputField _startingFloorInput;
+    [SerializeField] private Button _nextFloorButton;
 
     public DebugOptions Options { get; private set; }
     private bool _wasDebugMenuOpenLastFrame = false;
+    private FloorManager _floorManager;
 
     private void Awake()
     {
+        _floorManager = FindAnyObjectByType<FloorManager>();
+
         if (_debugBuildText)
         {
             _debugBuildText.SetActive(DEBUG);
@@ -41,12 +50,14 @@ public class DebugMenu : MonoBehaviour
             Options = JsonUtils.Load<DebugOptions>(PATH);
         }
         Options ??= new();
+    }
 
-        _canDie.isOn = Options.CanDie;
-
-        // Buttons
+    private void Start()
+    {
         // Player
         Player player = FindAnyObjectByType<Player>();
+        _canDie.SetIsOnWithoutNotify(Options.CanDie);
+        _canDie.onValueChanged.AddListener(on => Options.CanDie = on);
         _levelUpButton.enabled = player != null;
         _levelUp10Button.enabled = player != null;
         _killPlayerButton.enabled = player != null;
@@ -54,10 +65,10 @@ public class DebugMenu : MonoBehaviour
         {
             _levelUpButton.onClick.AddListener(() => { print("[DebugMenu] Levelled player up once"); player.PlayerLevel.ForceLevelUp(); });
             _levelUp10Button.onClick.AddListener(() =>
-                {
-                    print("[DebugMenu] Levelled player up 10 times");
-                    for (int i = 0; i < 10; i++) player.PlayerLevel.ForceLevelUp();
-                });
+            {
+                print("[DebugMenu] Levelled player up 10 times");
+                for (int i = 0; i < 10; i++) player.PlayerLevel.ForceLevelUp();
+            });
 
             _killPlayerButton.onClick.AddListener(() => { print("[DebugMenu] Damaged player for " + player.EntityStats.MaxHealth); LivingEntity.Damage(player, player.EntityStats.MaxHealth); });
         }
@@ -68,6 +79,48 @@ public class DebugMenu : MonoBehaviour
         // Scenes
         _goToGameButton.onClick.AddListener(() => { print("[DebugMenu] Loaded game scene"); SceneManager.LoadScene("Game", LoadSceneMode.Single); });
         _goToMenuButton.onClick.AddListener(() => { print("[DebugMenu] Loaded main menu scene"); SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); });
+
+        // Floors
+        _nextFloorButton.onClick.AddListener(() => { _floorManager.NextFloor(); });
+
+        _startOnFloorToggle.SetIsOnWithoutNotify(Options.StartOnFloor);
+        _startOnFloorToggle.onValueChanged.AddListener(on => Options.StartOnFloor = on);
+        Options.StartingFloor = Mathf.Max(0, Options.StartingFloor);
+        _startingFloorInput.SetTextWithoutNotify(Options.StartingFloor.ToString());
+        _startingFloorInput.onValueChanged.AddListener(newText =>
+        {
+            // Get rid of any spaces, etc
+            string cleanedInput = new string(newText
+            .Where(c => !char.IsWhiteSpace(c) && !char.IsControl(c) && c != '\u200B')
+            .ToArray());
+
+            // Parse as int
+            if (int.TryParse(cleanedInput, out int startingFloor))
+            {
+                if (startingFloor >= 0)
+                {
+                    Options.StartingFloor = startingFloor;
+                    print($"[DebugMenu] You will start on floor {startingFloor} next time you launch the game. (assuming the checkbox is checked)");
+                }
+                else
+                {
+                    Debug.LogWarning($"[DebugMenu] Starting floor ({startingFloor}) cannot be negative, using {Options.StartingFloor} as the starting floor.");
+                }
+            }
+            else
+            {
+                Debug.LogWarning($"[DebugMenu] Failed to parse '{cleanedInput}' as an integer");
+            }
+        });
+
+        if (DEBUG && Options.StartOnFloor && Options.StartingFloor != 0)
+        {
+            print("[DebugMenu] Starting on floor " + Options.StartingFloor);
+            while (_floorManager.Floor != Options.StartingFloor)
+            {
+                _floorManager.NextFloor();
+            }
+        }
     }
 
     private void OnDestroy()
@@ -92,7 +145,5 @@ public class DebugMenu : MonoBehaviour
         }
 
         if (!_canvas.activeInHierarchy) return;
-
-        Options.CanDie = _canDie.isOn;
     }
 }
